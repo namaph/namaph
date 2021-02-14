ef23b635a7eb8392f819102a402a94cabf5d67ac
takesei <sei06k14@gmail.com>
2021-02-15 01:27:38 +0900

    :sparkles: Add Roppongi2045 and Umekita Simulation

--- example-notebooks/Ecosystem/utils.py
+++ example-notebooks/Ecosystem/utils.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pandas as pd
+from celluloid import Camera
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-from typing import Tuple
+from typing import Tuple, List
 
 from .types import Ecosystem
 
@@ -18,7 +21,7 @@ def default_ecosystem(
   spc = np.zeros((num_species, *field_shape))
   return Ecosystem(res, spc, base_resource)
 
-def set_random(field: np.ndarray, cpf: int, ran=[10,50], spec:int = 100) -> np.ndarray:
+def set_random(field: np.ndarray, cpf: int, ran=[10,50]) -> np.ndarray:
   spec, col, row = field.shape
   temp = field
   for _ in range(cpf):
@@ -30,3 +33,28 @@ def set_random(field: np.ndarray, cpf: int, ran=[10,50], spec:int = 100) -> np.n
     init_val = np.random.randint(ran[0], ran[1], (spec))
     temp[tuple(map(tuple, init_pos.T))] += init_val
   return temp
+
+def set_scale(field: np.ndarray, plant: pd.DataFrame, cpf: int, scale:int=100) -> np.ndarray:
+  spec, col, row = field.shape
+  temp = field
+  for _ in range(cpf):
+    init_pos = np.hstack((
+      np.arange(spec)[:, None],
+      np.random.randint(0, col, (spec, 1)),
+      np.random.randint(0, row, (spec, 1))
+    ))
+    init_val = ((1 / plant.g) * 10)
+    temp[tuple(map(tuple, init_pos.T))] += init_val
+  return temp
+
+
+def animate(data: List[np.ndarray], fname:str):
+  fig = plt.figure()
+  camera = Camera(fig)
+
+  for i in data:
+      plot = sns.heatmap(i, cbar=False)
+      camera.snap()
+
+  anim = camera.animate(blit=False)
+  anim.save(fname, writer='imagemagick')
